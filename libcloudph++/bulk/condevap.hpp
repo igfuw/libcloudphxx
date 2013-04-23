5777c19de90ec022a7fb65f0dd6e0b92a0184a4f
trontrytel <ajaruga@igf.fuw.edu.pl>
2013-04-23 17:34:08 +0200

    adding first draft of bulk::condevap

---
+++ libcloudph++/bulk/condevap.hpp
@@ -0,0 +1,183 @@
+/** @file
+  * @copyright University of Warsaw
+  * @brief saturation adjustment routine using Boost.odeint for
+  *        solving latent-heat release equation
+  * @section LICENSE
+  * GPLv3+ (see the COPYING file or http://www.gnu.org/licenses/)
+  */
+
+#pragma once
+
+#include <boost/numeric/odeint.hpp>
+
+#include <libcloudph++/common/units.hpp>
+#include <libcloudph++/common/phc_const_cp.hpp>
+#include <libcloudph++/common/phc_theta.hpp>
+
+namespace libcloudphxx
+{
+  namespace bulk
+  {
+    namespace detail
+    {
+      // ODE rhs describing latent-heat release
+      template <typename real_t>
+      class rhs
+      {
+	private: 
+         
+        quantity<si::mass_density, real_t> rhod;
+	
+        public: 
+        
+        void init(
+	  const quantity<si::mass_density, real_t> _rhod,
+	  const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> rhod_th,
+	  const quantity<si::mass_density, real_t> rhod_rv
+	)
+	{
+	  rhod = _rhod;
+	  update(rhod_th, rhod_rv);
+	}
+
+	quantity<phc::mixing_ratio, real_t> r, rs;
+	quantity<si::pressure, real_t> p;
+	quantity<si::temperature, real_t> T;
+
+	private: 
+
+        void update(
+	  const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> rhod_th,
+	  const quantity<si::mass_density, real_t> rhod_rv
+	)
+	{
+	  r = rhod_rv / rhod;
+	  p = phc::p<real_t>(rhod_th, r);
+	  T = phc::T<real_t>(rhod_th / rhod, p, r);
+	  rs = phc::r_vs<real_t>(T, p);
+	}
+
+	public: 
+
+	// F = d (rho_d * th) / d (rho_d * r) = rho_d ()
+        void operator()(
+	  const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> rhod_th,
+	  quantity<si::temperature, real_t> &F,
+	  const quantity<si::mass_density, real_t> rhod_rv
+	)
+	{
+	  update(rhod_th, rhod_rv);
+	  F = phc::dtheta_drv<real_t>(T, p, r, rhod_th, rhod); // TODO: option : which dtheta...
+	}
+      };
+    }    
+
+    template <class container_t, typename real_t>
+    void condevap(
+//      const container_t &rhod,    // TODO: 
+      container_t &rhod_th, 
+      container_t &rhod_rv,
+      container_t &rhod_rc,
+      container_t &rhod_rr
+    )
+    {
+      // odeint::euler< // TODO: opcja?
+      namespace odeint = boost::numeric::odeint;
+
+      odeint::runge_kutta4<
+	quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t>, // state_type
+	real_t, // value_type
+	quantity<si::temperature, real_t>, // deriv_type
+	quantity<si::mass_density, real_t>, // time_type
+	odeint::vector_space_algebra,
+	odeint::default_operations,
+	odeint::never_resizer
+      > S; // TODO: would be better to instantiate in the ctor (but what about thread safety! :()
+      //typename detail::rhs F;
+
+      for (auto elem : rhod_th)
+      {
+std::cerr << elem << std::endl;
+      }
+/*
+    for (int k = rhod.lbound(mtx::k); k <= rhod.ubound(mtx::k); ++k)
+      for (int j = rhod.lbound(mtx::j); j <= rhod.ubound(mtx::j); ++j)
+        for (int i = rhod.lbound(mtx::i); i <= rhod.ubound(mtx::i); ++i)
+        {
+
+          F.init(
+            rhod(i,j,k) * si::kilograms / si::cubic_metres,
+            rhod_th(i,j,k) * si::kilograms / si::cubic_metres * si::kelvins,
+            rhod_rv(i,j,k) * si::kilograms / si::cubic_metres
+          );
+          real_t // TODO: quantity<si::mass_density
+            rho_eps = .00002, // TODO: as an option?
+            vapour_excess;
+          real_t drho_rr_max = 0; // TODO: quantity<si::mass_density
+          if (F.rs > F.r && rhod_rr(i,j,k) > 0 && opt_revp)
+            drho_rr_max = (dt / si::seconds) * (1 - F.r / F.rs) * (1.6 + 124.9 * pow(1e-3 * rhod_rr(i,j,k), .2046)) * pow(1e-3 * rhod_rr(i,j,k), .525) /
+              (5.4e2 + 2.55e5 * (1. / (F.p / si::pascals) / F.rs)); // TODO: move to phc!!!
+          bool incloud;
+
+          // TODO: rethink and document 2*rho_eps!!!
+          while (
+            // condensation of cloud water if supersaturated
+            (vapour_excess = rhod_rv(i,j,k) - rhod(i,j,k) * F.rs) > rho_eps
+            || (opt_cevp && vapour_excess < -rho_eps && ( // or if subsaturated
+              (incloud = (rhod_rc(i,j,k) > 0)) // cloud evaporation if in cloud
+              || (opt_revp && rhod_rr(i,j,k) > 0) // or rain evaportation if in a rain shaft (and out-of-cloud)
+            ))
+          )
+          {
+            real_t drho_rv = - copysign(.5 * rho_eps, vapour_excess);
+            drho_rv = (vapour_excess > 0 || incloud)
+              ? std::min(rhod_rc(i,j,k), drho_rv)
+              : std::min(drho_rr_max, std::min(rhod_rr(i,j,k), drho_rv)); // preventing negative mixing ratios
+            assert(drho_rv != 0); // otherwise it should not pass the while condition!
+
+            // theta is modified by do_step, and hence we cannot pass an expression and we need a temp. var.
+            quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t>
+              tmp = rhod_th(i,j,k) * si::kilograms / si::cubic_metres * si::kelvins;
+
+            // integrating the First Law for moist air
+            S.do_step(
+              boost::ref(F),
+              tmp,
+              rhod_rv(i,j,k) * si::kilograms / si::cubic_metres,
+              drho_rv * si::kilograms / si::cubic_metres
+            );
+
+            // latent heat source/sink due to evaporation/condensation
+            rhod_th(i,j,k) = tmp / (si::kilograms / si::cubic_metres * si::kelvins);
+
+            // updating rhod_rv
+            rhod_rv(i,j,k) += drho_rv;
+            assert(rhod_rv(i,j,k) >= 0);
+            assert(isfinite(rhod_rv(i,j,k)));
+            
+            if (vapour_excess > 0 || incloud)
+            {
+              rhod_rc(i,j,k) -= drho_rv; // cloud water
+              assert(rhod_rc(i,j,k) >= 0);
+              assert(isfinite(rhod_rc(i,j,k)));
+            }
+            else // or rain water
+            {
+              assert(opt_revp); // should be guaranteed by the while() condition above
+              rhod_rr(i,j,k) -= drho_rv;
+              assert(rhod_rr(i,j,k) >= 0);
+              assert(isfinite(rhod_rr(i,j,k)));
+              if ((drho_rr_max -= drho_rv) == 0) break; // but not more than Kessler allows
+            }
+          }
+          // hopefully true for RK4
+          assert(F.r == real_t(rhod_rv(i,j,k) / rhod(i,j,k)));
+          // double-checking....
+          assert(rhod_rc(i,j,k) >= 0);
+          assert(rhod_rv(i,j,k) >= 0);
+          assert(rhod_rr(i,j,k) >= 0);
+        }
+*/
+    }
+  }
+};
--- libcloudph++/common/phc_theta.hpp
+++ libcloudph++/common/phc_theta.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <libcloudph++/common/phc.hpp>
 #include <libcloudph++/common/phc_const_cp.hpp>
 
@@ -39,5 +41,56 @@ namespace phc
   {
     return (p - phc::p_v<real_t>(p, rv)) /
       (phc::exner<real_t>(p, rv) * phc::R_d<real_t>() * th);
+  }
+
+  // temperature as a function theta, pressure and water vapour mixing ratio for moist air
+  phc_declare_funct_macro quantity<si::temperature, real_t> T(
+    quantity<si::temperature, real_t> th, // TODO: const
+    quantity<si::pressure, real_t> p, // TODO: const
+    quantity<mixing_ratio, real_t> r // TODO: const
+  )
+  {
+    return th * exner<real_t>(p, r);
+  }
+
+
+  // pressure as a function of "theta times dry air density" and water vapour mixing ratio
+  phc_declare_funct_macro quantity<si::pressure, real_t> p(
+    const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> rhod_th,
+    quantity<mixing_ratio, real_t> r // TODO: const
+  )
+  {
+    return p_1000<real_t>() * real_t(pow(
+      (rhod_th * R_d<real_t>())
+        / p_1000<real_t>() * (real_t(1) + r / eps<real_t>()),
+      1 / (1 - R_over_c_p(r))
+    ));
+  }
+
+  // dtheta^star_drv from First Law for theta^star
+  // TODO: document the derivation!
+  phc_declare_funct_macro quantity<si::temperature, real_t> dtheta_drv(
+    const quantity<si::temperature, real_t> T,
+    const quantity<si::pressure, real_t> p,
+    const quantity<mixing_ratio, real_t> r,
+    const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> rhod_th,
+    const quantity<si::mass_density, real_t> rhod
+  )
+  {
+    return - rhod_th / rhod * (
+      // the 'liquid water' term
+      l_v<real_t>(T)
+        / real_t(pow(1 + r, 2))
+        / c_p(r)
+        / T
+/*
++
+// the 'virtual' term (TODO: as an option!)
+log(p / p_1000<real_t>())
+* R_d_over_c_pd<real_t>()
+* (real_t(1) / eps<real_t>() - real_t(1) / ups<real_t>())
+* real_t(pow(real_t(1) + r / ups<real_t>(),-2))
+*/
+    );
   }
 };
