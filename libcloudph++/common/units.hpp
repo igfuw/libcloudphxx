c7433dd181c3a08d7fb8ec7b8893957f70fb4fc7
trontrytel <ajaruga@igf.fuw.edu.pl>
2013-05-07 15:54:28 +0200

    wip on lcph_kessler

--- libcloudph++/common/units.hpp
+++ libcloudph++/common/units.hpp
@@ -5,6 +5,7 @@
 # include <boost/units/io.hpp>
 namespace si = boost::units::si;
 using boost::units::quantity;
+using boost::units::one;
 using boost::units::pow;
 using boost::units::root;
 using boost::units::multiply_typeof_helper;
