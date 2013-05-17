0f1b79319e8e0d70b98dcbd2a0d3f43e3ad4eb10
slayoo <slayoo@igf.fuw.edu.pl>
2013-05-17 19:57:29 +0200

    finishing switch from theta^star to theta_dry

--- libcloudph++/common/theta.hpp
+++ libcloudph++/common/theta.hpp
@@ -2,7 +2,8 @@
 
 #include <libcloudph++/common/const_cp.hpp>
 
-// \theta = (p_1000 / p_dry^{R_d / c_{pd}})
+// theta dry: \theta = (p_1000 / p_dry)^{R_d / c_{pd}}
+// normal theta : \theta = (p_1000 / p)^{R_d / c_{pd}}
 
 namespace libcloudphxx
 {
@@ -10,53 +11,31 @@ namespace libcloudphxx
   {
     namespace theta
     {
-      // TODO: we use dry air here!
       using moist_air::R;
       using moist_air::R_d;
       using moist_air::R_v;
-      using moist_air::eps;
       using moist_air::R_d_over_c_pd;
-      using moist_air::c_p;
       using moist_air::c_pd;
       using moist_air::p_v;
 
       // pressure in the definition of potential temperature
       libcloudphxx_declare_const_macro(p_1000, 100000, si::pascals)
 
-      // Exner function exponent for moist air
-      libcloudphxx_declare_funct_macro quantity<si::dimensionless, real_t> R_over_c_p(
-	const quantity<si::dimensionless, real_t> &r
-      )
-      {
-	return R<real_t>(r) / c_p<real_t>(r);
-      }
-
-
-      // Exner function for moist air
-      libcloudphxx_declare_funct_macro quantity<si::dimensionless, real_t> exner(
-	const quantity<si::pressure, real_t> &p,
-	const quantity<si::dimensionless, real_t> &r
-      )
-      {
-	return pow(p / p_1000<real_t>(), R_over_c_p<real_t>(r));
-      }
-
-
       // dry air density as a function of p, theta and rv
       libcloudphxx_declare_funct_macro quantity<si::mass_density, real_t> rhod(
 	const quantity<si::pressure, real_t> &p,
-	const quantity<si::temperature, real_t> &th, // 
+	const quantity<si::temperature, real_t> &th, // normal theta !!! TODO: document / separate file?
 	const quantity<si::dimensionless, real_t> &rv
       )
       {
 	return (p - p_v<real_t>(p, rv)) /
-	  (exner<real_t>(p, rv) * R_d<real_t>() * th);
+	  (pow(p / p_1000<real_t>(), R_d_over_c_pd<real_t>()) * R_d<real_t>() * th);
       }
 
 
       // TODO: eq. no
       libcloudphxx_declare_funct_macro quantity<si::temperature, real_t> T(
-        const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> &rhod_th,
+        const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> &rhod_th, // theta dry!!!
         const quantity<si::mass_density, real_t> &rhod
       ) {
         return si::kelvins * pow(
@@ -81,7 +60,7 @@ namespace libcloudphxx
       // see eq. TODO
       libcloudphxx_declare_funct_macro quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> d_rhodtheta_d_rv(
 	const quantity<si::temperature, real_t> &T,
-	const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> &rhod_th
+	const quantity<multiply_typeof_helper<si::mass_density, si::temperature>::type, real_t> &rhod_th // theta dry!!!
       )
       {
 	return - rhod_th * const_cp::l_v<real_t>(T) / c_pd<real_t>() / T;
