26a5816182d8ce287e45b8b67a7782fd1f05dcb5
trontrytel <ajaruga@igf.fuw.edu.pl>
2013-07-29 13:24:58 +0200

    adding empty formulae and forcing_elementwise files to blk_2m

---
+++ include/libcloudph++/blk_2m/formulae.hpp
@@ -0,0 +1,24 @@
+/** @file
+  * @copyright University of Warsaw
+  * @brief double-moment bulk parameterisation formulae
+  * @section LICENSE
+  * GPLv3+ (see the COPYING file or http://www.gnu.org/licenses/)
+  */
+
+#pragma once
+
+#include <libcloudph++/common/moist_air.hpp>
+
+namespace libcloudphxx
+{
+  namespace blk_2m
+  {
+    namespace formulae
+    {
+
+
+
+
+    };
+  };
+};
