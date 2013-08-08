87bcb8f214c9eb961d9e5c7667e4832c140b32ec
slayoo <slayoo@igf.fuw.edu.pl>
2013-08-08 17:40:44 +0200

    adding bisect test

---
+++ tests/bisect/test_bisect.cpp
@@ -0,0 +1,14 @@
+#include <libcloudph++/common/detail/bisect.hpp>
+
+#include <iostream>
+
+double f(double x)
+{
+  return x*x - 2;
+}
+
+int main()
+{
+  double a=1, b=2, tol=.001;
+  std::cerr << libcloudphxx::common::detail::bisect(f, a, b, tol) << std::endl;
+}
