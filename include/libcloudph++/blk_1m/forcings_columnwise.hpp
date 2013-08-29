847879eed32f5c1d60e490bf8f5f6de3fc62da0d
slayoo <sarabas@igf.fuw.edu.pl>
2013-08-29 12:30:24 +0200

    fixing iterator usage in forcings_columnwise to allow blitz to be used with debug mode

--- include/libcloudph++/blk_1m/forcings_columnwise.hpp
+++ include/libcloudph++/blk_1m/forcings_columnwise.hpp
@@ -38,8 +38,10 @@ namespace libcloudphxx
       const real_t *rhod, *rhod_rr = &zero;
 
       auto iter = zip(drhod_rr_cont, rhod_cont, rhod_rr_cont);
-      for (auto tup_ptr = --iter.end(); tup_ptr != --iter.begin(); --tup_ptr)
+      for (auto tup_ptr = iter.end(); tup_ptr != iter.begin();)
       {
+        --tup_ptr;
+
         const real_t
           *rhod_below     = &boost::get<1>(*tup_ptr),
           *rhod_rr_below  = &boost::get<2>(*tup_ptr);
