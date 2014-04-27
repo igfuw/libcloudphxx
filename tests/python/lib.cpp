3e64f48b549900e8649206a3c8a576a38a2a77f2
slayoo <sarabas@igf.fuw.edu.pl>
2014-04-27 21:38:03 +0200

    wip on the Python bindings

--- tests/python/lib.cpp
+++ tests/python/lib.cpp
@@ -7,6 +7,14 @@
 
 #include <libcloudph++/blk_1m/options.hpp>
 #include <libcloudph++/blk_1m/adj_cellwise.hpp>
+#include <libcloudph++/blk_1m/rhs_cellwise.hpp>
+#include <libcloudph++/blk_1m/rhs_columnwise.hpp>
+
+#include <libcloudph++/blk_2m/options.hpp>
+#include <libcloudph++/blk_2m/rhs_cellwise.hpp>
+#include <libcloudph++/blk_2m/rhs_columnwise.hpp>
+
+#include <libcloudph++/lgrngn/options.hpp>
 
 using real_t = double;
 namespace bp = boost::python;
@@ -19,27 +27,153 @@ struct iterable
   bp::stl_input_iterator<real_t> end() const { return bp::stl_input_iterator<real_t>(); }
 };
 
-void adj_cellwise(
-  const libcloudphxx::blk_1m::opts_t<real_t>& opts,
-  bp::object &rhod,
-  bp::object &th,
-  bp::object &rv,
-  bp::object &rc,
-  bp::object &rr,
-  const real_t &dt
-)
+namespace blk_1m
 {
-  iterable th_c(th), rv_c(rv), rc_c(rc), rr_c(rr);
-  libcloudphxx::blk_1m::adj_cellwise<real_t, iterable>(
-    opts, 
-    iterable(rhod), 
-    th_c, 
-    rv_c, 
-    rc_c, 
-    rr_c, 
-    dt
-  );
-}
+  void adj_cellwise(
+    const libcloudphxx::blk_1m::opts_t<real_t>& opts,
+    bp::object &rhod,
+    bp::object &th,
+    bp::object &rv,
+    bp::object &rc,
+    bp::object &rr,
+    const real_t &dt
+  )
+  {
+    iterable 
+      th_c(th),
+      rv_c(rv),
+      rc_c(rc),
+      rr_c(rr);
+    libcloudphxx::blk_1m::adj_cellwise<real_t, iterable>(
+      opts, 
+      iterable(rhod), // since it is const, it may be a temporary object
+      th_c, 
+      rv_c, 
+      rc_c, 
+      rr_c, 
+      dt
+    );
+  }
+
+  void rhs_cellwise(
+    const libcloudphxx::blk_1m::opts_t<real_t> &opts,
+    bp::object &dot_rc,
+    bp::object &dot_rr,
+    bp::object &rc,
+    bp::object &rr
+  ) 
+  {
+    iterable 
+      dot_rc_c(dot_rc), 
+      dot_rr_c(dot_rr);
+    libcloudphxx::blk_1m::rhs_cellwise(
+      opts,
+      dot_rc_c,
+      dot_rr_c,
+      iterable(rc),
+      iterable(rr)
+    );
+  } 
+
+  void rhs_columnwise(
+    const libcloudphxx::blk_1m::opts_t<real_t> &opts,
+    bp::object &dot_rr,
+    bp::object &rhod,
+    bp::object &rr,
+    const real_t &dz
+  ) {
+    iterable 
+      dot_rr_c(dot_rr);
+// TODO: fails to compile (iterators)
+//    libcloudphxx::blk_1m::rhs_columnwise<real_t, iterable>(
+//      opts,
+//      dot_rr_c,
+//      iterable(rhod),
+//      iterable(rr),
+//      dz
+//    );
+  } 
+};
+
+namespace blk_2m
+{
+  void rhs_cellwise(
+    const libcloudphxx::blk_2m::opts_t<real_t> &opts,
+    bp::object &dot_th,
+    bp::object &dot_rv,
+    bp::object &dot_rc,
+    bp::object &dot_nc,
+    bp::object &dot_rr,
+    bp::object &dot_nr,
+    bp::object &rhod,
+    bp::object &th,
+    bp::object &rv,
+    bp::object &rc,
+    bp::object &nc,
+    bp::object &rr,
+    bp::object &nr,
+    const real_t dt
+  ) 
+  {
+    iterable 
+      dot_th_c(dot_rc), 
+      dot_rv_c(dot_rc), 
+      dot_rc_c(dot_rc), 
+      dot_nc_c(dot_rc), 
+      dot_rr_c(dot_rc), 
+      dot_nr_c(dot_rr);
+// TODO: fails to compile (min() not declared for iterable)
+/*
+    libcloudphxx::blk_2m::rhs_cellwise(
+      opts,
+      dot_th_c,
+      dot_rv_c,
+      dot_rc_c,
+      dot_nc_c,
+      dot_rr_c,
+      dot_nr_c,
+      iterable(rhod),
+      iterable(th),
+      iterable(rv),
+      iterable(rc),
+      iterable(nc),
+      iterable(rr),
+      iterable(nr),
+      dt
+    );
+*/
+  } 
+
+  void rhs_columnwise(
+    const libcloudphxx::blk_2m::opts_t<real_t> &opts,
+    bp::object &dot_rr,
+    bp::object &dot_nr,
+    bp::object &rhod,
+    bp::object &rr,
+    bp::object &nr,
+    const real_t &dt,
+    const real_t &dz
+  ) {
+    iterable 
+      dot_rr_c(dot_rr), 
+      dot_nr_c(dot_nr);
+// TODO: fails to compile (iterators)
+//    libcloudphxx::blk_2m::rhs_columnwise<real_t, iterable>(
+//      opts,
+//      dot_rr_c,
+//      dot_nr_c,
+//      iterable(rhod),
+//      iterable(rr),
+//      iterable(nr),
+//      dt,
+//      dz
+//    );
+  } 
+};
+
+namespace lgrngn
+{
+};
 
 BOOST_PYTHON_MODULE(libcloudphxx)
 {
@@ -56,12 +190,28 @@ BOOST_PYTHON_MODULE(libcloudphxx)
     bp::scope().attr("blk_1m") = nested_module;
     bp::scope parent = nested_module;
     bp::class_<libcloudphxx::blk_1m::opts_t<real_t>>("opts_t");
-    bp::def("adj_cellwise", adj_cellwise);
+    bp::def("adj_cellwise", blk_1m::adj_cellwise);
+    bp::def("rhs_cellwise", blk_1m::rhs_cellwise); 
+    bp::def("rhs_columnwise", blk_1m::rhs_columnwise); 
   }
 
   // blk_2m stuff
-  {} // TODO
+  {
+    std::string nested_name = bp::extract<std::string>(bp::scope().attr("__name__") + ".blk_2m");
+    bp::object nested_module(bp::handle<>(bp::borrowed(PyImport_AddModule(nested_name.c_str()))));
+    bp::scope().attr("blk_2m") = nested_module;
+    bp::scope parent = nested_module;
+    bp::class_<libcloudphxx::blk_2m::opts_t<real_t>>("opts_t");
+    bp::def("rhs_cellwise", blk_2m::rhs_cellwise);
+    bp::def("rhs_columnwise", blk_2m::rhs_columnwise);
+  } 
 
   // lgrngn stuff
-  {} // TODO
+  {
+    std::string nested_name = bp::extract<std::string>(bp::scope().attr("__name__") + ".lgrngn");
+    bp::object nested_module(bp::handle<>(bp::borrowed(PyImport_AddModule(nested_name.c_str()))));
+    bp::scope().attr("lgrngn") = nested_module;
+    bp::scope parent = nested_module;
+    bp::class_<libcloudphxx::lgrngn::opts_t<real_t>>("opts_t");
+  }
 }
--- tests/python/test.py
+++ tests/python/test.py
@@ -1,8 +1,9 @@
 import sys
 sys.path.append(".")
 
-from libcloudphxx import blk_1m
+from libcloudphxx import blk_1m, blk_2m, lgrngn
 
+# blk_1m
 opts = blk_1m.opts_t()
 opts.cond = True
 opts.cevp = True
@@ -12,10 +13,16 @@ opts.accr = True
 opts.sedi = False
 
 rhod = [1]
-th   = [270]
+th   = [300]
 rv   = [0]
 rc   = [0]
 rr   = [0]
 dt   = 1
 
 blk_1m.adj_cellwise(opts, rhod, th, rv, rc, rr, dt)
+
+# blk_2m
+opts = blk_2m.opts_t()
+
+# lgrngn
+opts = lgrngn.opts_t()
