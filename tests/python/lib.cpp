27abf01c4e04a825a086fe54e094065163b73b9f
slayoo <slayoo@igf.fuw.edu.pl>
2014-05-07 10:43:38 +0200

    adding a sanity check for contiguous memory layout in np2bz() (Python binding)

--- tests/python/lib.cpp
+++ tests/python/lib.cpp
@@ -27,15 +27,23 @@ using arr_t = blitz::Array<real_t, 1>;
 
 inline arr_t np2bz(bp::numeric::array &arg)
 {
-// TODO: asserts on shape?
+  // assuring double precision
   if (std::string(bp::extract<std::string>(arg.attr("dtype").attr("name"))) != "float64")
     throw std::runtime_error("dtype=float64 required for all passed arrays");
 
+  // assuring contiguous layout
+  if (!bp::extract<bool>(arg.attr("flags").attr("c_contiguous")))
+    throw std::runtime_error("contiguous memory layout required");
+
+  // wrapping the data into a Blitz++ array to get STL-container-like functionality
   return arr_t(
+    // pointer to the data
     reinterpret_cast<real_t*>(
       (long)bp::extract<long>(arg.attr("ctypes").attr("data"))
     ), 
+    // length of the array (regardless of the original dimensionality, we do 1D)
     blitz::shape(bp::extract<long>(arg.attr("size"))), 
+    // ensure Blitz++ does not try to free the memory when done
     blitz::neverDeleteData
   );
 }
