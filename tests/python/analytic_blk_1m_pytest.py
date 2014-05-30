3b69f25e0a7e2ace33ca0a9732b70e2a4b511723
Dorota Jarecka <djarecka@gmail.com>
2014-05-29 18:27:06 -0600

    adding file with analytical equations used to calculate the expected values for tests (tests don't call it automatically)

---
+++ tests/python/analytic_blk_1m_pytest.py
@@ -0,0 +1,30 @@
+import numpy as np
+import math
+
+from constants_pytest import es0, T0, Rv, Rd, cp, L, p0
+
+#in Pa (4.23 C&W)
+def press_sat(T, es_ref=es0, T_ref=T0):
+    return es_ref * math.exp(-L/Rv * (1./T - 1./T_ref))
+
+# in 1 (4.37 C&W)
+def mixrat_sat(T, p, es_ref=es0, T_ref=T0):
+    es = press_sat(T)
+    return Rd/Rv * es / (p-es)
+
+def pot_temp(T,p):
+    return T * (p0 / p)**(Rd/cp)
+
+#linearization of: r_v - \delta r_v = r_vs(\theta + \delta \theta) and
+# \delta \theta = L/c_p * \theta/T 
+def delta_r(rv, T, p, es_ref=es0, T_ref=T0):
+    rvs = mixrat_sat(T,p)
+    return (rv - rvs) / (1. + rvs / T**2 * L**2 / Rv / cp)
+    
+
+print mixrat_sat(283.15, 900.e2)
+print pot_temp(283.15, 900.e2)
+print press_sat(283.15, 1227., 283.15)
+print delta_r(10.e-3, 283.15, 900.e2)
+print delta_r(8.e-3, 283.15, 900.e2)
+print delta_r(9.e-3, 283.15, 900.e2)
