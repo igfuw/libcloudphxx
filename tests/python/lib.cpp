0a167ebda05dedae5cf28b7b87bb0fca73bbddf6
djarecka <djarecka@gmail.com>
2014-06-15 06:39:27 +0200

    correction to the binding of theta_dry::T and adding theta_dry::p

--- tests/python/lib.cpp
+++ tests/python/lib.cpp
@@ -362,7 +362,13 @@ namespace common
   template <typename real_t>
   real_t T(const real_t &th, const real_t &rhod)
   {
-    return cmn::theta_dry::T(th * si::kelvins, rhod) / si::kelvins;
+    return cmn::theta_dry::T(th * si::kelvins, rhod  * si::kilograms / si::cubic_metres) / si::kelvins;
+  }
+
+  template <typename real_t>
+  real_t p(const real_t &rhod, const real_t &r, const real_t &T)
+  {
+    return cmn::theta_dry::p(rhod  * si::kilograms / si::cubic_metres, r * si::kilograms / si::kilograms, T * si::kelvins) / si::pascals;
   }
 
 
@@ -392,6 +398,7 @@ BOOST_PYTHON_MODULE(libcloudphxx)
 
     bp::def("p_vs", &common::p_vs<real_t>);
     bp::def("T", &common::T<real_t>);
+    bp::def("p", &common::p<real_t>);
   }
 
   // blk_1m stuff
