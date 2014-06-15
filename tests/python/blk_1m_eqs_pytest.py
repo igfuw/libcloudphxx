6ec90a92dd13af3ad3ff38877f052ba0cb264ab4
djarecka <djarecka@gmail.com>
2014-06-15 06:40:18 +0200

    adding some simple tests of consistency between equation used in the libcloudph model and in python tests

--- tests/python/blk_1m_eqs_pytest.py
+++ tests/python/blk_1m_eqs_pytest.py
@@ -6,20 +6,34 @@ import pdb
 
 from numpy import array as arr_t
 import analytic_blk_1m_pytest as anpy
+from libcloudphxx  import common
 
-#TODO from libcloudphxx_blk_1m_pytest import adj_cellwise
+"""it checks consistency between thermodynamical equations used in the libcloudph and my python tests""" 
 
-def theta_model_test(temp,pres):
-    return 273.
-
-@pytest.mark.parametrize("arg, expected", [
-        ({"T" : arr_t([273.]),   "p" : arr_t([1.e5])}, {"theta" : arr_t([0.])}),
-        ({"T" : arr_t([283.15]), "p" : arr_t([9.e4])}, {"theta" : arr_t([0.])})
+@pytest.mark.parametrize("arg", [
+    ({"T" : 273.,   "press" : 1.e5,  "rv" : 0.}), ({"T" : 273.,   "press" : 1.e5,  "rv" : 1.e-2}),
+    ({"T" : 283.15, "press" : 9.e4,  "rv" : 0.}), ({"T" : 283.15, "press" : 9.e4,  "rv" : 1.e-2})
     ])
-def test_theta(arg, expected, epsilon = 0.05):
-    #TODO
-    theta_md = theta_model_test(arg["T"], arg["p"])
-    theta_anpy = anpy.pot_temp(**arg)
-    assert abs(theta_anpy - theta_md) <= epsilon * theta_anpy
+def test_thetadry(arg, epsilon = 0.01):
+    rho_d = anpy.density_dry(**arg)
+    th_d = anpy.pottemp_dry(**arg)
+    temp_md = common.T(th_d, rho_d)
+    assert abs(arg["T"] - temp_md) <= epsilon * arg["T"]
 
 
+@pytest.mark.parametrize("arg", [
+    ({"T" : 273.,   "press" : 1.e5,  "rv" : 0.}), ({"T" : 273.,   "press" : 1.e5,  "rv" : 1.e-2}),
+    ({"T" : 283.15, "press" : 9.e4,  "rv" : 0.}), ({"T" : 283.15, "press" : 9.e4,  "rv" : 1.e-2})
+        ])
+def test_pressure(arg, epsilon = 0.01):
+    rho_d = anpy.density_dry(**arg)
+    press_md = common.p(rho_d, arg["rv"], arg["T"])
+    assert abs(arg["press"] - press_md) <= epsilon * arg["press"]
+                    
+
+@pytest.mark.parametrize("arg", [{"T" : 273.}, {"T" : 283.}, {"T" : 293.}])
+def test_pvs(arg, epsilon = 0.03):
+    pvs_md = common.p_vs(arg["T"])
+    pvs_anpy = anpy.press_sat(arg["T"])
+    assert abs(pvs_anpy - pvs_md) <= epsilon * pvs_anpy
+                    
