40973fd27fc1aec6f9ed49f96319c8121cb62abf
Dorota Jarecka <djarecka@gmail.com>
2014-06-18 13:18:28 -0600

    Delete constants_pytest.py

--- tests/python/constants_pytest.py
+++
@@ -1,8 +0,0 @@
-# some constants - should be moved and probably unified...TODO!
-es0 = 6.107e2 #Pa (Curry&Webster)
-T0 = 273.15 #K
-Rv = 461.4 #J/kg/K  (libcloud)
-Rd = 287.1 #J/kg/K (libcloud)
-cp = 1005. #J/kg/K (libcloud)
-L = 2.5e6 #J/kg (libcloud)
-p0 = 1000.e2 #Pa 
