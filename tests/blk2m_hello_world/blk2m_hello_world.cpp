cfae2247f6c7eeaf7234946404bdbfe0efec3700
slayoo <slayoo@igf.fuw.edu.pl>
2015-01-23 11:21:35 +0100

    fixinf fortran memory order, adding halo regions, inserting some meaningfull values to test the output

--- tests/blk2m_hello_world/blk2m_hello_world.cpp
+++ tests/blk2m_hello_world/blk2m_hello_world.cpp
@@ -10,21 +10,35 @@
 
 namespace b2m = libcloudphxx::blk_2m;
 using real_t = double;
-
-const size_t nx = 64, nz = 64;
+const int 
+  n_dims = 2,
+  n_halo = 1,
+  nx = 4, 
+  nz = 4;
 const real_t dt = 1, dz = 10;
 
 // MicroHH-style (reastrict + ijk Fortran index order)
 using ptr_t = std::unique_ptr<real_t restrict>;
 
-blitz::Array<real_t, 2> bz(const ptr_t &ptr)
+blitz::Array<real_t, n_dims> bz(const ptr_t &ptr)
 {
-  return blitz::Array<real_t, 2>(
-    *ptr, 
-    blitz::shape(nx, nz), 
-    blitz::FortranArray<2>()
-);
+  return blitz::Array<real_t, n_dims>(
+    ptr.get(), 
+    blitz::shape(
+      nx + 2 * n_halo, 
+      nz + 2 * n_halo
+    ), 
+    blitz::neverDeleteData,
+    blitz::ColumnMajorArray<n_dims>()
+  ).reindex({
+    -n_halo, 
+    -n_halo
+  });
 }
+
+blitz::Range 
+  i_all(0, nx-1), 
+  j_all(0, nz-1);
 
 int main()
 {
@@ -36,7 +50,7 @@ int main()
   
   // memory allocation
   for (auto arr : std::set<ptr_t*>({ &rho_d, &rr, &nr, &dot_rr, &dot_nr }))
-    arr->reset(new real_t[nx*nz]);
+    arr->reset(new real_t[ (nx+2*n_halo) * (nz+2*n_halo) ]);
 
   // instantiating the options container
   b2m::opts_t<real_t> opts;  
@@ -50,17 +64,33 @@ int main()
     bz_dot_nr = bz(dot_nr)
   ;
 
+  // some meaningfull values
+  bz_rho_d(i_all, j_all) = 1;
+  bz_rr(i_all, nz-1) = 1e-5;  
+  bz_nr(i_all, nz-1) = 1e3;  
+
+  std::cerr << bz_rr << std::endl;
+
   // calling the blk_2m methods from libcloudph++ 
-  b2m::rhs_columnwise(
-    opts,
-    bz_dot_rr,
-    bz_dot_nr,
-    bz_rho_d,
-    bz_rr,
-    bz_nr,
-    dt,
-    dz
-  );
+  for (int i=0; i < nx; ++i)
+  {
+    // columns of dotted fields to be modified
+    auto 
+      bz_dot_rr_ij = bz_dot_rr(i, j_all),
+      bz_dot_nr_ij = bz_dot_nr(i, j_all);
+    b2m::rhs_columnwise(
+      opts,
+      bz_dot_rr_ij,
+      bz_dot_nr_ij,
+      bz_rho_d  (i, j_all),
+      bz_rr     (i, j_all),
+      bz_nr     (i, j_all),
+      dt,
+      dz
+    );
+  }
+
+  std::cerr << bz_dot_rr << std::endl;
 
   //b2m::rhs_cellwise(); // TODO
 }
