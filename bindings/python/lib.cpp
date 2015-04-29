8063c27d630ddee7eb65c8c4342ca397d99f1f34
slayoo <slayoo@igf.fuw.edu.pl>
2015-04-29 10:25:15 +0200

    adding p_hydro to Python bindings

--- bindings/python/lib.cpp
+++ bindings/python/lib.cpp
@@ -57,6 +57,7 @@ BOOST_PYTHON_MODULE(libcloudphxx)
     bp::def("p", &common::p<real_t>);
     bp::def("rw3_cr", &common::rw3_cr<real_t>);
     bp::def("S_cr", &common::S_cr<real_t>);
+    bp::def("p_hydro", &common::p_hydro<real_t>);
   }
 
   // blk_1m stuff
