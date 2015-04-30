20582a7d9de2c617f05b91b1706486ad026f1f4a
slayoo <slayoo@igf.fuw.edu.pl>
2015-04-30 15:53:52 +0200

    adding parcel_plot.py, and calculation of RH in parcel.py

--- tests/python/physics/parcel.py
+++ tests/python/physics/parcel.py
@@ -7,6 +7,8 @@ import inspect, numpy as np
 import pdb
 
 def micro_init(opts, state):
+  # sanity check
+  if (state["RH"] > 1): raise Exception("Please supply initial T,p,r_v below supersaturation")
 
   # using nested function to get access to opts
   def lognormal(lnr):
@@ -27,23 +29,27 @@ def micro_init(opts, state):
   micro.init(state["th_d"], state["r_v"], state["rhod"])
   return micro
 
-def micro_step(micro, state):
+def micro_step(micro, state, info):
   opts = lgrngn.opts_t()
   micro.step_sync(opts, state["th_d"], state["r_v"], state["rhod"]) 
 
-def save_init(outfile, state):
+def stats(state, info):
+  state["T"] = np.array([common.T(state["th_d"][0], state["rhod"][0])])
+  state["RH"] = state["p"] * state["r_v"] / (state["r_v"] + common.eps) / common.p_vs(state["T"][0])
+  info["RH_max"] = max(info["RH_max"], state["RH"])
+
+def output_init(outfile, state):
   f = netcdf.netcdf_file(outfile, 'w')
   f.createDimension('t', None)
   
-  units = {"z" : "m", "t" : "s", "r_v" : "kg/kg", "th_d" : "K", "rhod" : "kg/m3", "p" : "Pa"}
+  units = {"z" : "m", "t" : "s", "r_v" : "kg/kg", "th_d" : "K", "rhod" : "kg/m3", "p" : "Pa", "T" : "K", "RH" : "1"}
 
   for var in state:
     f.createVariable(var, 'd', ('t',))
     f.variables[var].unit = units[var]
-  save_record(f, state)
   return f
 
-def save_record(f, state):
+def output_save(f, state):
   rec = f.variables['t'].shape[0]
   f.variables['t'][rec] = state["t"]
   for var, val in state.iteritems():
@@ -53,7 +59,7 @@ def save_attrs(f, dictnr):
   for var, val in dictnr.iteritems():
     setattr(f, var, val)
 
-def parcel(dt=.1, z_max=100, w=1, T_0=300, p_0=101300, r_0=.05, outfile="test.nc", outfreq=100, sd_conc_mean=64, kappa=.5,
+def parcel(dt=.1, z_max=2000, w=1, T_0=300, p_0=101300, r_0=.022, outfile="test.nc", outfreq=100, sd_conc_mean=64, kappa=.5,
   mean_r = .04e-6 / 2, stdev  = 1.4, n_tot  = 60e6
 ):
   # packing function arguments into "opts" dictionary
@@ -66,10 +72,17 @@ def parcel(dt=.1, z_max=100, w=1, T_0=300, p_0=101300, r_0=.05, outfile="test.nc
     "t" : 0, "z" : 0,
     "r_v" : np.array([r_0]), "p" : p_0,
     "th_d" : np.array([common.th_std2dry(th_0, r_0)]), 
-    "rhod" : np.array([common.rhod(p_0, th_0, r_0)])
+    "rhod" : np.array([common.rhod(p_0, th_0, r_0)]),
+    "T" : None, "RH" : None
   }
-  with save_init(outfile, state) as f:
+  info = { "RH_max" : 0 }
+  with output_init(outfile, state) as f:
+    # t=0 : init & save
+    stats(state, info)
     micro = micro_init(opts, state)
+    output_save(f, state)
+
+    # timestepping
     for it in range(1,nt+1):
       # diagnostics
       # the reasons to use analytic solution:
@@ -81,14 +94,16 @@ def parcel(dt=.1, z_max=100, w=1, T_0=300, p_0=101300, r_0=.05, outfile="test.nc
       state["rhod"][0] = common.rhod(state["p"], th_0, r_0)
 
       # microphysics
-      micro_step(micro, state)
+      micro_step(micro, state, info)
+      stats(state, info)
+
+      # TODO: only if user wants to stop @ RH_max
+      #if (state["RH"] < info["RH_max"]): break
 
       # output
-      if (it % outfreq == 0): save_record(f, state)
+      if (it % outfreq == 0): output_save(f, state)
 
-      # stats
-
-    #save_stats(f, )
+    save_attrs(f, info)
     save_attrs(f, opts)
 
 parcel()
