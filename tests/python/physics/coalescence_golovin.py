33685a920761ed292d3610077d3f88a1953ab339
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2015-06-11 17:10:08 +0200

    relax coalescence golo test to match new init

--- tests/python/physics/coalescence_golovin.py
+++ tests/python/physics/coalescence_golovin.py
@@ -122,5 +122,5 @@ rmsd = RMSD(results,golovin_results)
 
 print 'RMSD = ' + str(rmsd);
 
-if(rmsd > 5.7e-6):
+if(rmsd > 7.7e-6):
   raise Exception("Simulation result does not agree with analytic prediction")
