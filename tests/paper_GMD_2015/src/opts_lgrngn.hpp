92a79d09ee7f7b813a85294cf029a14cbc9cce2a
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2015-07-28 13:54:50 +0200

    hardcode n_sd_max in icicle

--- tests/paper_GMD_2015/src/opts_lgrngn.hpp
+++ tests/paper_GMD_2015/src/opts_lgrngn.hpp
@@ -61,6 +61,7 @@ void setopts_micro(
   rt_params.async = vm["async"].as<bool>();
 
   rt_params.cloudph_opts_init.sd_conc = vm["sd_conc"].as<unsigned long long>();
+  rt_params.cloudph_opts_init.n_sd_max = vm["sd_conc"].as<unsigned long long>() * nx * nz;
   rt_params.cloudph_opts_init.nx = nx;
   rt_params.cloudph_opts_init.nz = nz;
   boost::assign::ptr_map_insert<
