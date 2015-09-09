00042ad93c252581902a34043a62730818f58601
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2015-09-09 17:45:01 +0200

    define multi_gpu constructor

--- src/particles_multi_gpu.cpp
+++ src/particles_multi_gpu.cpp
@@ -1,16 +1,62 @@
 // contains definitions of members of particles_t specialized for multiple GPUs
+#include <omp.h>
+
+// macro to check for cuda errors, taken from 
+// http://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+#define gpuErrchk(ans) { detail::gpuAssert((ans), __FILE__, __LINE__); }
 
 namespace libcloudphxx
 {
   namespace lgrngn
   {
+    namespace detail
+    {
+      inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+      {
+         if (code != cudaSuccess) 
+         {
+            fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+            if (abort) exit(code);
+         }
+      }
+    };
 
     // constructor
     template <typename real_t>
-    particles_t<real_t, multi_CUDA>::particles_t(const opts_init_t<real_t> &)
+    particles_t<real_t, multi_CUDA>::particles_t(const opts_init_t<real_t> &opts_init)
     {
-//    int dev_count; // number of GPUs used
-//    std::vector<particles_t<real_t, CUDA> *> particles; // pointer to particles_t on each GPU
+      // get number of available devices
+      gpuErrchk(cudaGetDeviceCount(&dev_count)); 
+      
+      // set number of devices to use
+      if(opts_init.dev_count > 0)
+      {
+        if(dev_count < opts_init.dev_count)
+          throw std::runtime_error("number of available GPUs smaller than number of GPUs defined in opts_init");
+        else 
+          dev_count = opts_init.dev_count;
+      }
+   
+      // check if all GPUs support UVA, TODO: move this to cmake
+      for (int i = 0; i < dev_count; ++i)
+      {
+        // Get device properties
+        cudaDeviceProp devProp;
+        gpuErrchk(cudaGetDeviceProperties(&devProp, i));
+        if(!devProp.unifiedAddressing)
+          throw std::runtime_error("One of the GPUs doesn't support Unified Virtual Addressing.");
+      }
+
+      // resize the pointer vector
+      particles.resize(dev_count);
+
+      // assign device to each thread and create particles_t in each
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int tid = omp_get_thread_num();
+        gpuErrchk(cudaSetDevice(tid));
+        particles.at(tid) = new particles_t<real_t, CUDA>(opts_init);
+      }
     }
 
     // initialisation 
@@ -29,7 +75,7 @@ namespace libcloudphxx
     // time-stepping methods
     template <typename real_t>
     void particles_t<real_t, multi_CUDA>::step_sync(
-      const opts_t<real_t> &,
+      const opts_t<real_t> &opts_init,
       arrinfo_t<real_t> th,
       arrinfo_t<real_t> rv,
       const arrinfo_t<real_t> courant_1,
@@ -42,7 +88,7 @@ namespace libcloudphxx
 
     template <typename real_t>
     real_t particles_t<real_t, multi_CUDA>::step_async(
-      const opts_t<real_t> &
+      const opts_t<real_t> &opts
     )
     {
     }
@@ -102,6 +148,11 @@ namespace libcloudphxx
 
     template <typename real_t>
     void particles_t<real_t, multi_CUDA>::diag_all()
+    {
+    }
+
+    template <typename real_t>
+    real_t* particles_t<real_t, multi_CUDA>::outbuf()
     {
     }
 
