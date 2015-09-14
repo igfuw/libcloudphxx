94bb857016c56cc9ab3e10a31095f1933e445e32
Piotr Dziekan <plgpdziekan@zeus.cyfronet.pl>
2015-09-14 14:41:13 +0200

    multi-gpu diag methods

--- src/particles_multi_gpu.cpp
+++ src/particles_multi_gpu.cpp
@@ -138,6 +138,11 @@ namespace libcloudphxx
     template <typename real_t>
     void particles_t<real_t, multi_CUDA>::diag_sd_conc()
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_sd_conc();
+      }
     }
 
     template <typename real_t>
@@ -145,6 +150,11 @@ namespace libcloudphxx
       const real_t &r_mi, const real_t &r_mx
     )
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_dry_rng(r_mi, r_mx);
+      }
     }
 
     template <typename real_t>
@@ -152,44 +162,84 @@ namespace libcloudphxx
       const real_t &r_mi, const real_t &r_mx
     )
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_wet_rng(r_mi, r_mx);
+      }
     }
 
     template <typename real_t>
     void particles_t<real_t, multi_CUDA>::diag_dry_mom(const int &k)
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_dry_mom(k);
+      }
     }
 
     template <typename real_t>
     void particles_t<real_t, multi_CUDA>::diag_wet_mom(const int &k)
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_wet_mom(k);
+      }
     }
 
     template <typename real_t>
-    void particles_t<real_t, multi_CUDA>::diag_wet_mass_dens(const real_t&, const real_t&)
+    void particles_t<real_t, multi_CUDA>::diag_wet_mass_dens(const real_t &a, const real_t &b)
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_wet_mass_dens(a, b);
+      }
     }
 
     // ...
 //</listing>
 
     template <typename real_t>
-    void particles_t<real_t, multi_CUDA>::diag_chem(const enum chem_species_t&)
+    void particles_t<real_t, multi_CUDA>::diag_chem(const enum chem_species_t &spec)
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_chem(spec);
+      }
     }
 
     template <typename real_t>
     void particles_t<real_t, multi_CUDA>::diag_rw_ge_rc()
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_rw_ge_rc();
+      }
     }
 
     template <typename real_t>
     void particles_t<real_t, multi_CUDA>::diag_RH_ge_Sc()
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_RH_ge_Sc();
+      }
     }
 
     template <typename real_t>
     void particles_t<real_t, multi_CUDA>::diag_all()
     {
+      #pragma omp parallel num_threads(dev_count)
+      {
+        const int dev_id = omp_get_thread_num();
+        particles[dev_id]->diag_all();
+      }
     }
 
     template <typename real_t>
