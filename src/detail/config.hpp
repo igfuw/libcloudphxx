29d293d60bf4d8987d4a481224ab416fb6781d6b
pdziekan <pdziekanp@gmail.com>
2016-09-15 12:40:55 +0200

    pre-c++11 compliant config initialization

--- src/detail/config.hpp
+++ src/detail/config.hpp
@@ -15,14 +15,16 @@ namespace libcloudphxx
          const uintmax_t n_iter = 100;      // number of iterations of the toms748 root-finding
 
         // precision of toms748
-         const common::detail::eps_tolerance<real_t> eps_tolerance = 
-          common::detail::eps_tolerance<real_t>(sizeof(real_t) * 8 / 4);
+         const common::detail::eps_tolerance<real_t> eps_tolerance;
 
          const real_t bin_precision = 1e-4; // size of bins in ln(radius) when calculating roots, integral, CDF
          const real_t threshold = 1e4;      // in constant multiplicity mode, min and max initial radii are
                                            // defined by n_lnrd(r_min/max) = (maximum of n_lnrd) / threshold
          const real_t rd_min_init = 1e-11, 
                      rd_max_init = 1e-3;   // bounding values for the initial dry radius distro
+        config():
+          eps_tolerance(sizeof(real_t) * 8 / 4)
+          {}
       };
     };
   };
