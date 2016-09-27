6871629f7f786d144640c8162c57cdc75eedd4d9
Anna Jaruga <ajaruga@igf.fuw.edu.pl>
2016-09-27 14:29:44 +0200

    print out 3rd moms in sstp test

--- tests/python/unit/test_sstp_cond.py
+++ tests/python/unit/test_sstp_cond.py
@@ -96,5 +96,7 @@ for sstp_cond in [1,2,5]:
   prtcls.diag_all()
   prtcls.diag_wet_mom(3);
   wet_post_adve_cond = frombuffer(prtcls.outbuf()).copy()
+  print wet_post_adve
+  print wet_post_adve_cond
   assert allclose(wet_post_adve, wet_post_adve_cond, atol=0, rtol=1e-5)
 
