1b97120bed9fa8f1fa939e46071d15fad109b114
trontrytel <ajaruga@igf.fuw.edu.pl>
2016-11-21 20:05:37 +0100

    testing chem loop

--- tests/paper_GMD_2015/tests/chem_sandbox/calc_chem.cpp
+++ tests/paper_GMD_2015/tests/chem_sandbox/calc_chem.cpp
@@ -32,41 +32,45 @@ int main(int ac, char** av)
     bins_wet_str = tmp.str();
   }
 
-  string opts_common = 
-    "--outfreq=200  --nt=9000 --spinup=7200 --nx=76 --nz=76 --relax_th_rv=false";
-  set<string> opts_micro({
-    "--micro=lgrngn_chem --outdir=out_lgrngn_chem --backend=CUDA --adv_serial=False --sd_conc=128 "
-                    "--sstp_cond=10 --sstp_chem=10 --coal=True --sedi=True " 
-                    "--chem_switch=True --chem_dsl=True --chem_dsc=True --chem_rho=1.8e3 "  
-                    //chem_rct switched on afetr spinup in set_chem  
-                    "--mean_rd1=0.05e-6 --sdev_rd1=1.8 --n1_stp=50e6 "
-                    "--mean_rd2=0.1e-6  --sdev_rd2=1.4 --n2_stp=0 "
-                    "--kernel=hall_davis_no_waals --terminal_velocity=beard77fast "
-                    "--SO2_g_0=.2e-9 --O3_g_0=50e-9 --H2O2_g_0=.5e-9 --CO2_g_0=360e-6 --NH3_g_0=.1e-9 --HNO3_g_0=0 "
-      " --out_wet=\""                                                                                //TODO 100e-12
-        ".5e-6:25e-6|0,1,2,3;" // FSSP
-        "25e-6:1|0,3;"         // "rain"
-        + bins_wet_str +       // aerosol spectrum (wet)
-      "\""
-      " --out_dry=\""
-        "0.:1.|0,1;"
-        + bins_dry_str +       // aerosol spectrum (dry)
-      "\""
-      " --out_chem=\""
-        "0:1|0;"               // chem spectrum (dry)
-      "\""
-      " --out_wet_pH=\""
-        + bins_wet_str +       // spectrum for S_VI and H+ (wet)
-      "\""
-  });
-
-  for (auto &opts_m : opts_micro)
+  for (const string &kernel : {"geometric", "long", "hall", "hall_davis_no_waals", "golovin", "onishi_hall", "onishi_hall_davis_no_waals", "vohl_davis_no_waals", "hall_pinsky_cumulonimbus", "hall_pinsky_stratocumulus"})
   {
-    ostringstream cmd;
-    cmd << av[1] << "/src/icicle " << opts_common << " " << opts_m;  
-    notice_macro("about to call: " << cmd.str())
 
-    if (EXIT_SUCCESS != system(cmd.str().c_str()))
-      error_macro("model run failed: " << cmd.str())
+    string opts_common = 
+      "--outfreq=200  --nt=9000 --spinup=7200 --nx=76 --nz=76 --relax_th_rv=false";
+    set<string> opts_micro({
+      "--micro=lgrngn_chem --outdir=out_"+kernel+" --backend=CUDA --adv_serial=False --sd_conc=256 "
+                      "--sstp_cond=10 --coal=True --sedi=True " 
+                      "--chem_switch=True --chem_dsl=True --chem_dsc=True --chem_rho=1.8e3 sstp_chem=10"  
+                      //chem_rct switched on afetr spinup in set_chem  
+                      "--mean_rd1=0.05e-6 --sdev_rd1=1.8 --n1_stp=50e6 "
+                      "--mean_rd2=0.1e-6  --sdev_rd2=1.4 --n2_stp=0 "
+                      "--kernel="+kernel+" --terminal_velocity=beard77fast "
+                      "--SO2_g_0=.2e-9 --O3_g_0=50e-9 --H2O2_g_0=.5e-9 --CO2_g_0=360e-6 --NH3_g_0=.1e-9 --HNO3_g_0=1e-10"
+        " --out_wet=\""
+          ".5e-6:25e-6|0,1,2,3;" // FSSP
+          "25e-6:1|0,3;"         // "rain"
+          + bins_wet_str +       // aerosol spectrum (wet)
+        "\""
+        " --out_dry=\""
+          "0.:1.|0,1;"
+          + bins_dry_str +       // aerosol spectrum (dry)
+        "\""
+  //      " --out_chem=\""
+  //        "0:1|0;"               // chem spectrum (dry)
+  //      "\""
+  //      " --out_wet_pH=\""
+  //        + bins_wet_str +       // spectrum for S_VI and H+ (wet)
+  //      "\""
+    });
+  
+    for (auto &opts_m : opts_micro)
+    {
+      ostringstream cmd;
+      cmd << av[1] << "/src/icicle " << opts_common << " " << opts_m;  
+      notice_macro("about to call: " << cmd.str())
+  
+      if (EXIT_SUCCESS != system(cmd.str().c_str()))
+        error_macro("model run failed: " << cmd.str())
+    }
   }
 }
