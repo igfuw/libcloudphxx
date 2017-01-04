04bf218fc3cc47cd3bf872e8f91c63e64faafd97
trontrytel <ajaruga@igf.fuw.edu.pl>
2017-01-04 12:35:50 +0100

    wip

--- tests/paper_GMD_2015/tests/chem_sandbox/plot_lgrngn_ch.cpp
+++ tests/paper_GMD_2015/tests/chem_sandbox/plot_lgrngn_ch.cpp
@@ -25,7 +25,7 @@ int main(int ac, char** av)
   
     auto n = h5n(h5);
 
-    for (int at = 36; at < n["t"]; ++at) // TODO: mark what time does it actually mean! 
+    for (int at = 0; at < n["t"]; ++at) // TODO: mark what time does it actually mean! 
     {
       for (auto &plt : std::set<std::string>({"rl", "rr", "nc", "nr", "ef", "na", "rd", "sd_conc", "th", "rv"})) 
       {
