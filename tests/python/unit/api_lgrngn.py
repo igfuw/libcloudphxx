6998fc24bda0a97bdf6d4de0deb1244f9f0c5ed4
pdziekan <pdziekanp@gmail.com>
2017-04-07 16:15:37 +0200

    add tests for 0D tail and const_multi

--- tests/python/unit/api_lgrngn.py
+++ tests/python/unit/api_lgrngn.py
@@ -123,6 +123,82 @@ prtcls.diag_kappa_mom(1)
 prtcls.diag_sd_conc()
 assert frombuffer(prtcls.outbuf()) == opts_init.sd_conc # parcel set-up
 
+# 0D with large_tail option
+print "0D large tail"
+opts_init.sd_conc_large_tail = 1
+prtcls = lgrngn.factory(backend, opts_init)
+prtcls.init(th, rv, rhod)
+
+prtcls.step_sync(opts, th, rv)
+prtcls.step_async(opts)
+opts_init.sd_conc_large_tail = 0
+prtcls.diag_sd_conc()
+
+assert len(frombuffer(prtcls.outbuf())) == 1
+print frombuffer(prtcls.outbuf())
+assert (frombuffer(prtcls.outbuf()) > 0).all()
+assert sum(frombuffer(prtcls.outbuf())) >= opts_init.sd_conc
+
+# 0D const multi - number of SDs and number of particles
+print "0D const multi"
+opts_init.sd_conc = 0
+prtcls_per_cell = 2 * n_tot / rho_stp #rhod=1; 2* because of two distributions
+opts_init.sd_const_multi = int(prtcls_per_cell / 64) 
+opts_init.n_sd_max = int(prtcls_per_cell / opts_init.sd_const_multi)
+prtcls = lgrngn.factory(backend, opts_init)
+prtcls.init(th, rv, rhod)
+prtcls.diag_sd_conc()
+
+prtcls.step_sync(opts, th, rv)
+prtcls.step_async(opts)
+prtcls.step_sync(opts, th, rv, rhod)
+prtcls.step_async(opts)
+prtcls.diag_sd_conc()
+
+assert len(frombuffer(prtcls.outbuf())) == 1
+print frombuffer(prtcls.outbuf())
+assert (frombuffer(prtcls.outbuf()) > 0).all()
+sd_tot = frombuffer(prtcls.outbuf()).sum()
+prtcls.diag_all()
+prtcls.diag_wet_mom(0)
+prtcls_tot = frombuffer(prtcls.outbuf()).sum()
+assert ((prtcls_tot / sd_tot)  == opts_init.sd_const_multi)
+
+# 0D dry_sizes init
+print "0D dry sizes"
+opts_init.dry_distros = dict()
+opts_init.dry_sizes = {kappa1 : {1.e-6 : 30. * rho_stp, 15.e-6 : 10. * rho_stp}}
+
+opts_init.sd_const_multi = 1
+opts_init.n_sd_max = int(40)
+prtcls = lgrngn.factory(backend, opts_init)
+prtcls.init(th, rv, rhod)
+
+prtcls.diag_sd_conc()
+print frombuffer(prtcls.outbuf())
+assert (frombuffer(prtcls.outbuf()) > 0).all()
+
+sd_tot = frombuffer(prtcls.outbuf()).sum()
+prtcls.diag_all()
+prtcls.diag_wet_mom(0)
+prtcls_tot = frombuffer(prtcls.outbuf()).sum()
+print frombuffer(prtcls.outbuf())
+assert ((prtcls_tot / sd_tot)  == opts_init.sd_const_multi)
+
+prtcls.diag_dry_rng(1e-6, 1.1e-6);
+prtcls.diag_wet_mom(0)
+print frombuffer(prtcls.outbuf())
+assert (frombuffer(prtcls.outbuf()) == 30 ).all()
+
+prtcls.diag_dry_rng(15e-6, 15.1e-6);
+prtcls.diag_wet_mom(0)
+print frombuffer(prtcls.outbuf())
+assert (frombuffer(prtcls.outbuf()) == 10 ).all()
+
+# go back got distros init
+opts_init.dry_sizes = dict()
+opts_init.dry_distros = {kappa1:lognormal, kappa2:lognormal}
+
 # 1D (periodic horizontal domain)
 print "1D"
 rhod = arr_t([  1.,   1.,   1.])
