5ef2aa8a6e6be2ddb8c0a5ab5f1276533ad3e5e0
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2017-10-19 12:18:08 +0200

    remove unary function's do_clone and new_clone - obsolete now

--- include/libcloudph++/common/unary_function.hpp
+++ include/libcloudph++/common/unary_function.hpp
@@ -14,15 +14,6 @@ namespace libcloudphxx
       virtual real_t funval(const real_t) const = 0;
       BOOST_GPU_ENABLED
       real_t operator()(const real_t arg) const { return funval(arg); }
- 
-      // to make Boost ptr_containers work
-      virtual unary_function * do_clone() const = 0;
     };
-
-    template <typename real_t>
-    inline unary_function<real_t> * new_clone(const unary_function<real_t>& o)
-    {
-      return o.do_clone();
-    }
   };
 };
