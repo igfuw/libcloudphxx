f0af7b214a5f2c16c635592b1fb72bd7de353e97
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2018-05-17 17:00:57 +0200

    test Newton-Raphson procedures also in api_blk_1m test

--- tests/python/unit/api_blk_1m.py
+++ tests/python/unit/api_blk_1m.py
@@ -47,12 +47,36 @@ assert rv != rv_old
 assert rc != rc_old
 assert rr == rr_old
 
+# sat adjustment using Newton-Raphson under constant pressure
+th   = arr_t([300.])
+rv   = arr_t([0.  ])
+rc   = arr_t([0.01])
+blk_1m.adj_cellwise_nwtrph(opts, p, p_d, th, rv, rc, dt)
+assert th != th_old # some water should have evaporated
+assert rv != rv_old
+assert rc != rc_old
+assert rr == rr_old
+
 dot_rc = arr_t([0.])
 dot_rr = arr_t([0.])
 blk_1m.rhs_cellwise(opts, dot_rc, dot_rr, rc, rr)
 assert dot_rc != 0 # some water should have coalesced
 assert dot_rr != 0
 
+# forcing using Newton-Raphson saturation adjustment include rain evaporation
+rr   = arr_t([0.01])
+dot_th = arr_t([0.])
+dot_rv = arr_t([0.])
+dot_rc = arr_t([0.])
+dot_rr = arr_t([0.])
+
+blk_1m.rhs_cellwise_nwtrph(opts, dot_th, dot_rv, dot_rc, dot_rr, rhod, p, p_d, th, rv, rc, rr)
+assert dot_rc != 0 # some water should have coalesced
+assert dot_rr != 0
+assert dot_th != 0 # some rain should have evaporated
+assert dot_rv != 0
+
+rr   = arr_t([0.])
 dot_rr_old = dot_rr.copy()
 flux = blk_1m.rhs_columnwise(opts, dot_rr, rhod, rr, dz)
 assert flux == 0
