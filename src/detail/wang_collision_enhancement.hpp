4c0a74f9c7e747451cbc56568a91d65c898b6843
pdziekan <pdziekanp@gmail.com>
2019-02-05 12:33:01 +0100

    wang coll enh with thrust vctrs

--- src/detail/wang_collision_enhancement.hpp
+++ src/detail/wang_collision_enhancement.hpp
@@ -11,17 +11,20 @@ namespace libcloudphxx
       template<class real_t>
       struct wang_collision_enhancement_t
       {
-        real_t R0[7];            // collector radius
-        real_t rat[11];          // ratio of radii
-        real_t eta_e[11][2][7];  // [rat][eps][R0]
+        static const int rat_size = 11, eps_size = 2, R0_size = 7;
+        thrust_device::vector<real_t> R0;     // collector radius
+        thrust_device::vector<real_t> rat;    // ratio of radii
+        thrust_device::vector<real_t> eta_e;  // [rat][eps][R0]
 
         // ctor
-        wang_collision_enhancement_t() 
+        BOOST_GPU_ENABLED
+        wang_collision_enhancement_t() :
+        R0(R0_size), rat(rat_size), eta_e(rat_size * eps_size * R0_size)
         {
           real_t R0_def[] = {10e-6, 20e-6, 30e-6, 40e-6, 50e-6, 60e-6, 100e-6};
-          std::copy(R0_def, R0_def+7, R0);
+          thrust::copy(R0_def, R0_def+R0_size, R0.begin());
           real_t rat_def[11] = {0., .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.};
-          std::copy(rat_def, rat_def+11, rat);
+          thrust::copy(rat_def, rat_def+rat_size, rat.begin());
           real_t eta_e_def[11][2][7] =  
           {                                       
             {{1.74, 1.74, 1.773,1.49, 1.207,1.207,1.0},
@@ -47,7 +50,13 @@ namespace libcloudphxx
             {{20.3, 20.3, 14.6, 8.61, 2.60, 2.60, 1.0},
             {36.52,36.52,19.16,22.80,26.0, 26.0, 1.0}}
           };
-          std::copy(&eta_e_def[0][0][0], &eta_e_def[0][0][0]+154, &eta_e[0][0][0]);
+          thrust::copy(&eta_e_def[0][0][0], &eta_e_def[0][0][0]+(rat_size * eps_size * R0_size), eta_e.begin());
+        }
+
+        BOOST_GPU_ENABLED
+        inline int get_eta_index(const int &n_rat, const int &n_eps, const int &n_R0) const
+        {
+          return n_rat * (eps_size + R0_size) + n_eps * (R0_size) + n_R0;
         }
 
         BOOST_GPU_ENABLED
@@ -69,7 +78,7 @@ namespace libcloudphxx
           for(n_rat = 1; n_rat < 11; ++n_rat)
             if(rat[n_rat] > ratio) break;
   
-          if(n_R0 == 0) return eta_e[n_rat][n_eps][n_R0];  // do not interpolate if both are smaller than 10um
+          if(n_R0 == 0) return eta_e[get_eta_index(n_rat, n_eps, n_R0)];  // do not interpolate if both are smaller than 10um
   
           // two-dimensional linear interpolation
           // note: we interpolate in (radius, ratio) space, not in (radius, radius)...
@@ -86,10 +95,10 @@ namespace libcloudphxx
   
           return
           (   
-            eta_e[n_rat-1][n_eps][n_R0-1] * w[1] * w[3] +
-            eta_e[n_rat-1][n_eps][n_R0]   * w[0] * w[3] +
-            eta_e[n_rat][n_eps][n_R0-1]   * w[1] * w[2] +
-            eta_e[n_rat][n_eps][ n_R0]     * w[0] * w[2]
+            eta_e[get_eta_index(n_rat-1, n_eps, n_R0-1)] * w[1] * w[3] +
+            eta_e[get_eta_index(n_rat-1, n_eps, n_R0  )] * w[0] * w[3] +
+            eta_e[get_eta_index(n_rat,   n_eps, n_R0-1)] * w[1] * w[2] +
+            eta_e[get_eta_index(n_rat,   n_eps, n_R0  )] * w[0] * w[2]
           ) / (R0[n_R0] - R0[n_R0-1])
             / (rat[n_rat]-rat[n_rat-1]); 
         }
