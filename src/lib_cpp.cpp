9eea4a3ac9fa5f66965f995feb8c8dd3d6ccb0a7
pdziekan <pdziekanp@gmail.com>
2019-02-06 15:29:04 +0100

    Revert "get rid of lib.hpp workarounds that messed with CUDA 9 and 10"
    This reverts commit 545454263ef2477fed12c7eca5e65f2966e3b17a.

--- src/lib_cpp.cpp
+++ src/lib_cpp.cpp
@@ -1,3 +1,8 @@
+#include "lib.hpp"
+
+// workarounding Thrust bug #383: (Thanks to Jared for suggestion!)
+#include <thrust/system/cpp/execution_policy.h>
+
 #include <thrust/system/cpp/vector.h>
 namespace thrust_device = ::thrust::cpp;
 
--- src/lib_omp.cpp
+++ src/lib_omp.cpp
@@ -1,3 +1,8 @@
+#include "lib.hpp"
+
+// workarounding Thrust bug #383 (Thanks to Jared for suggestion!)
+#include <thrust/system/omp/execution_policy.h>
+
 #include <thrust/system/omp/vector.h>
 namespace thrust_device = ::thrust::omp;
 
