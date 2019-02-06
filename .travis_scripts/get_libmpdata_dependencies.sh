4bffb425e46ea4bac5f62460cbac65bca329a2cf
pdziekan <pdziekanp@gmail.com>
2019-02-06 13:59:55 +0100

    Revert "add brew update to travis mpdata deps"
    This reverts commit 9625703d658c5275814a436a5934404b4e3e7e41.

--- .travis_scripts/get_libmpdata_dependencies.sh
+++ .travis_scripts/get_libmpdata_dependencies.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then brew install gnuplot; fi
-
 # gnuplot-iostream
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install gnuplot-nox; fi
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then brew install gnuplot; fi
