bc49856be4d278d9fb817d40fc871fe31f057a36
pdziekan <pdziekanp@gmail.com>
2019-02-06 15:38:01 +0100

    disable hack for bug #383 and hack for bug #382 on CUDA

--- src/lib_cpp.cpp
+++ src/lib_cpp.cpp
@@ -1,7 +1,7 @@
 #include "lib.hpp"
 
 // workarounding Thrust bug #383: (Thanks to Jared for suggestion!)
-#include <thrust/system/cpp/execution_policy.h>
+//#include <thrust/system/cpp/execution_policy.h>
 
 #include <thrust/system/cpp/vector.h>
 namespace thrust_device = ::thrust::cpp;
