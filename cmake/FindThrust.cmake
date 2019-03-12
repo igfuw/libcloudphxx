09d085fb13edc74c4ba64a065d2278ed8e3135a5
pdziekan <pdziekanp@gmail.com>
2019-03-12 13:57:54 +0100

    add the VTKm license file for FindThrust

--- cmake/FindThrust.cmake
+++ cmake/FindThrust.cmake
@@ -2,7 +2,7 @@
 ##
 ##  Copyright (c) Kitware, Inc.
 ##  All rights reserved.
-##  See LICENSE.txt for details.
+##  See LICENSE_VTKm.txt for details.
 ##
 ##  This software is distributed WITHOUT ANY WARRANTY; without even
 ##  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
