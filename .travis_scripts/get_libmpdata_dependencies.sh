d00931a129813ea59ffe612c3dce57e986268458
pdziekan <pdziekanp@gmail.com>
2019-05-29 14:43:32 +0200

    travis: icicle test - install hdf5 tools

--- .travis_scripts/get_libmpdata_dependencies.sh
+++ .travis_scripts/get_libmpdata_dependencies.sh
@@ -8,6 +8,7 @@ sudo wget -O /usr/local/include/gnuplot-iostream.h https://raw.githubusercontent
 
 #if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libhdf5-7; fi
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install  -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" libpango-1.0-0 libpangocairo-1.0-0 libhdf5-dev; fi
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install cmake; fi #libgnuplot-iostream-dev 
 #if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libhdf5-serial-dev hdf5-tools cmake; fi #libgnuplot-iostream-dev 
 # https://github.com/travis-ci/travis-ci/issues/8826
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then brew install hdf5; fi
