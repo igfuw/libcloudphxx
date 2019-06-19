1c312c9760c82bfda2b25d915dc2985a913ecdb0
pdziekan <pdziekanp@gmail.com>
2019-06-19 11:05:22 +0200

    compile multicuda only if more than 1 gpu available

--- src/lib_multicuda.cu
+++ src/lib_multicuda.cu
@@ -9,7 +9,7 @@
 #include <thrust/system/cuda/vector.h>
 namespace thrust_device = ::thrust::cuda;
 
-#include "particles.tpp"
+#include "particles_multi_gpu.tpp"
 
 namespace libcloudphxx
 { 
@@ -20,8 +20,8 @@ namespace libcloudphxx
     {   
     }  
 
-    // instantiation 
-    template class particles_t<float, CUDA>;
-    template class particles_t<double, CUDA>; 
+    // TODO: move these to other file added if cmake detects more than 1 GPU?
+    template class particles_t<float, multi_CUDA>;
+    template class particles_t<double, multi_CUDA>; 
   };
 };
