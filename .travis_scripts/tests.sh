d206c9290aa7b4cca3ff3fb30210cd0ecb09bd74
pdziekan <pdziekanp@gmail.com>
2019-07-19 16:04:58 +0200

    travis tests: try to go without sudo

--- .travis_scripts/tests.sh
+++ .travis_scripts/tests.sh
@@ -10,10 +10,10 @@ cmake -DCMAKE_BUILD_TYPE=Debug ../
 VERBOSE=1 make
 OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
 # make with RelWithDebInfo to have high optimization with asserts on
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ 
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=~/usr/local/ ../ 
 VERBOSE=1 make 
 OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
-sudo make install
+make install
 cd ../..
 
 ## drops.py (it is written in Python so no compilation, just unit tests)
@@ -21,6 +21,7 @@ cd ../..
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libhdf5-dev; fi
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install python-h5py; fi 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then git clone --depth=1 git://github.com/igfuw/drops.py.git; fi
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then export PYTHONPATH=${HOME}/usr/local/lib64/python2.7/site-packages/:${PYTHONPATH}; fi
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then cd drops.py; fi
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then mkdir build; fi
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then cd build; fi
