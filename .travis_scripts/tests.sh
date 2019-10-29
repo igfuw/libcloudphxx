ed2972b3673cb559c281b1216718449e0ad1dd88
pdziekan <pdziekanp@gmail.com>
2019-10-29 14:03:37 +0100

    travis tests: wait 20 and make on 2 threads

--- .travis_scripts/tests.sh
+++ .travis_scripts/tests.sh
@@ -7,11 +7,11 @@ cd build
 # if [[ $TRAVIS_OS_NAME == 'linux' && $CXX == 'clang++' ]]; then cmake ../; fi 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then cmake .. -DPYTHON_LIBRARY=${PY_LIB} -DPYTHON_INCLUDE_DIR=${PY_INC}; fi
 cmake -DCMAKE_BUILD_TYPE=Debug ../
-VERBOSE=1 make
+travis_wait 20 VERBOSE=1 make -j2
 OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
 # make with RelWithDebInfo to have high optimization with asserts on
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ 
-VERBOSE=1 make 
+travis_wait 20 VERBOSE=1 make -j2
 OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
 sudo make install
 cd ../..
