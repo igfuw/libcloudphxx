7f04adf0c022766cdecfd1909d15d9f34f76325c
pdziekan <pdziekanp@gmail.com>
2019-11-19 14:21:22 +0100

    add puddle names map

--- include/libcloudph++/lgrngn/output.hpp
+++ include/libcloudph++/lgrngn/output.hpp
@@ -18,5 +18,19 @@ namespace libcloudphxx
       outliq_vol,
       outdry_vol
     };
+
+    const std::map<output_t, std::string> output_names
+    {
+      {outHNO3    = "HNO3"}, 
+      {outNH3     = "NH3"}, 
+      {outCO2     = "CO2"}, 
+      {outSO2     = "SO2"}, 
+      {outH2O2    = "H2O2"}, 
+      {outO3      = "O3"},
+      {outS_VI    = "S_VI"},                        
+      {outH       = "H"},                          
+      {outliq_vol = "liquid_volume"},
+      {outdry_vol = "dry_volume"}
+    };
   };
 };
