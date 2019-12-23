8d63027eefb7ac6c5962baa22fc08b36fb79f641
pdziekan <pdziekanp@gmail.com>
2019-12-23 11:52:38 +0100

    travis tests.sh: dobt run tests of drops - drops repo was removed as obsoleted by parcel

--- .travis_scripts/tests.sh
+++ .travis_scripts/tests.sh
@@ -15,16 +15,4 @@ VERBOSE=1 make
 OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
 sudo make install
 cd ../..
-
-## drops.py (it is written in Python so no compilation, just unit tests)
-# only on linux
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libhdf5-dev; fi
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install python-h5py; fi 
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then git clone --depth=1 git://github.com/igfuw/drops.py.git; fi
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then cd drops.py; fi
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then mkdir build; fi
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then cd build; fi
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then  cmake ..; fi
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then make test || cat Testing/Temporary/LastTest.log /; fi # "/" intentional! (just to make cat exit with an error code)
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then  cd ../..; fi
 set +ex # see https://github.com/travis-ci/travis-ci/issues/6522
