9184677e9574e06d9595c593bf96d75d93e49593
pdziekan <pdziekanp@gmail.com>
2020-04-25 13:41:32 +0200

    travis mt: use pre-c++17 gnuplot-iostream

--- .travis_scripts/get_libmpdata_dependencies.sh
+++ .travis_scripts/get_libmpdata_dependencies.sh
@@ -9,7 +9,11 @@ git checkout 8b6e30c6ea5ee4f07ccf90d858d35a10cf67a3e2 # later commits require c+
 sudo cp gnuplot-iostream.h /usr/local/include/gnuplot-iostream.h
 cd ..
 
-sudo wget -O /usr/local/include/gnuplot-iostream.h https://raw.githubusercontent.com/dstahlke/gnuplot-iostream/master/gnuplot-iostream.h
+# gnuplot-iostream
+git clone -n https://github.com/dstahlke/gnuplot-iostream && cd gnuplot-iostream
+git checkout 8b6e30c6ea5ee4f07ccf90d858d35a10cf67a3e2 # later commits require c++17
+sudo cp gnuplot-iostream.h /usr/local/include/gnuplot-iostream.h
+cd ..
 
 #if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libhdf5-7; fi
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install  -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" libpango-1.0-0 libpangocairo-1.0-0 libhdf5-dev; fi
