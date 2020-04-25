970b6b4d5203895fc0da40badbb140c8da183547
pdziekan <pdziekanp@gmail.com>
2020-04-25 15:48:25 +0200

    travis mpdata deps: install iostream only once

--- .travis_scripts/get_libmpdata_dependencies.sh
+++ .travis_scripts/get_libmpdata_dependencies.sh
@@ -3,11 +3,6 @@ set -e
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install gnuplot-nox; fi
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then brew install gnuplot; fi
-# gnuplot-iostream
-git clone -n https://github.com/dstahlke/gnuplot-iostream && cd gnuplot-iostream
-git checkout 8b6e30c6ea5ee4f07ccf90d858d35a10cf67a3e2 # later commits require c++17
-sudo cp gnuplot-iostream.h /usr/local/include/gnuplot-iostream.h
-cd ..
 
 # gnuplot-iostream
 git clone -n https://github.com/dstahlke/gnuplot-iostream && cd gnuplot-iostream
