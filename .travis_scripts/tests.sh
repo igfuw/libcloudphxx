7c62068078dd2056d7aa2db0ef42cbcef8a46d75
pdziekan <pdziekanp@gmail.com>
2020-05-14 20:19:13 +0200

    travis: use newer cmake:

--- .travis_scripts/tests.sh
+++ .travis_scripts/tests.sh
@@ -6,11 +6,11 @@ mkdir build
 cd build
 # if [[ $TRAVIS_OS_NAME == 'linux' && $CXX == 'clang++' ]]; then cmake ../; fi 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then cmake .. -DPYTHON_LIBRARY=${PY_LIB} -DPYTHON_INCLUDE_DIR=${PY_INC} -DBoost_NO_BOOST_CMAKE=ON; fi
-cmake -DCMAKE_BUILD_TYPE=Debug ../
+$cmake -DCMAKE_BUILD_TYPE=Debug ../
 VERBOSE=1 make
 OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
 # make with RelWithDebInfo to have high optimization with asserts on
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ 
+$cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ 
 VERBOSE=1 make 
 OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
 sudo make install
