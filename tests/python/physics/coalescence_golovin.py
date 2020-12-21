23fbb64b198b5fbc0a94ceba95ef2834c2f054ed
pdziekan <pdziekanp@gmail.com>
2020-12-21 14:37:57 +0100

    coalescence golovin test - increase tolerance a little

--- tests/python/physics/coalescence_golovin.py
+++ tests/python/physics/coalescence_golovin.py
@@ -136,7 +136,7 @@ for i in range(0,2):
   
   if(i==0):
     print('sd_conc RMSD = ' + str(rmsd));
-    limit = 1e-5;
+    limit = 1.1e-5;
   else:
     print('const_multi RMSD = ' + str(rmsd));
     limit = 3e-5; # constant multiplicity doesn't represent tails of the distribution so well and mass densty function depends on large tail?
