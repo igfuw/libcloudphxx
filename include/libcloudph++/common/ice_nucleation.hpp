e5b41e63786e51e3ee289d356fe122c1e00a39fe
Agnieszka Makulska <a.makulska@student.uw.edu.pl>
2025-07-14 11:39:31 +0200

    sampling in src/ with multiple random numbers

--- include/libcloudph++/common/ice_nucleation.hpp
+++ include/libcloudph++/common/ice_nucleation.hpp
@@ -10,24 +10,19 @@ namespace libcloudphxx
     {
       enum class INP_t {mineral}; // types of ice nucleating particles, TODO: add more types
 
-      // Freezing temperature as defined in Shima et al., 2020
+      // Inverse CDF for freezing temperature as defined in eq. 1 in Shima et al., 2020
       template <typename real_t>
       BOOST_GPU_ENABLED
-      quantity<si::temperature, real_t> T_freeze(
+      quantity<si::temperature, real_t> T_freeze_CDF_inv(
       const INP_t& INP_type,                          // type of ice nucleating particle
       const quantity<si::length, real_t> rd3_insol,    // radius cubed of ice nucleating (insoluble) particle
-      int rng_seed                                    // seed for random number generator
+      const real_t rand                                    // random number between [0, 1]
         ) {
         real_t A = real_t(4) * pi<real_t>() * std::pow(rd3_insol/si::meters, 2/3); // surface area of the insoluble particle
 
-        std::mt19937 gen(rng_seed); // random number generator
-        std::uniform_real_distribution<> dis(0, 1);
-        real_t Y = dis(gen); // random number between [0, 1]
-
-        // Sampling T_freez from its CDF using inverse transform sampling
-        if (INP_type == INP_t::mineral && A > std::numeric_limits<real_t>::epsilon() && dis(gen) > 0.05) // 5% of mineral dust particles are inactive
+        if (INP_type == INP_t::mineral && A > std::numeric_limits<real_t>::epsilon())
         {
-          return (real_t(273.15) + (real_t(8.934) - std::log(- std::log(1-Y) / A) ) / real_t(0.517)) * si::kelvins;
+          return (real_t(273.15) + (real_t(8.934) - std::log(- std::log(1 - rand) / A) ) / real_t(0.517)) * si::kelvins;
         }
         else
         {
@@ -37,16 +32,24 @@ namespace libcloudphxx
 
 
       template<typename real_t>
-      struct T_freeze_functor
+      struct T_freeze_CDF_inv_functor
       {
         INP_t INP_type;
-        int rng_seed;
-        T_freeze_functor(INP_t INP_type, int rng_seed)
-          : INP_type(INP_type), rng_seed(rng_seed) {}
+
+        T_freeze_CDF_inv_functor(INP_t INP_type)
+          : INP_type(INP_type) {}
+
         BOOST_GPU_ENABLED
-        real_t operator()(const real_t &rd3_insol) const
+        real_t operator()(const thrust::tuple<real_t, real_t> &tpl) const
         {
-          return ice_nucleation::T_freeze<real_t>(INP_type, rd3_insol * si::meters, rng_seed).value();
+          const real_t &rd3_insol = thrust::get<0>(tpl);  // from rd3 vector
+          const real_t &rand         = thrust::get<1>(tpl);  // from rand vector
+
+          return ice_nucleation::T_freeze_CDF_inv<real_t>(
+            INP_type,
+            rd3_insol * si::meters,
+            rand
+          ).value();
         }
       };
 
--- src/impl/particles_impl_init_T_freeze.ipp
+++ src/impl/particles_impl_init_T_freeze.ipp
@@ -6,26 +6,27 @@
   * @brief initialisation routine for super droplets
   */
 
-#include "../../include/libcloudph++/common/ice_nucleation.hpp"
+#include <libcloudph++/common/ice_nucleation.hpp>
 
 namespace libcloudphxx
 {
   namespace lgrngn
   {
     template <typename real_t, backend_t device>
-  void particles_t<real_t, device>::impl::init_T_freeze(
-    const int rng_seed
-    )
-    {
+    void particles_t<real_t, device>::impl::init_T_freeze() {
+
       using namespace libcloudphxx::common::ice_nucleation;
 
       const INP_t inp_type = INP_t::mineral; //TODO: INP type as argument, to support different partcle types
 
+      // random numbers between [0,1] for sampling
+      rand_u01(n_part_to_init);
+
       thrust::transform(
-        rd3_insol.begin() + n_part_old,
-        rd3_insol.end(),
+        thrust::make_zip_iterator(thrust::make_tuple(rd3_insol.begin() + n_part_old, u01.begin() + n_part_old)),
+        thrust::make_zip_iterator(thrust::make_tuple(rd3_insol.end(),   u01.end())),
         T_freeze.begin() + n_part_old,
-        T_freeze_functor<real_t>(inp_type, rng_seed)
+        T_freeze_CDF_inv_functor<real_t>(inp_type)
       );
     }
   };
