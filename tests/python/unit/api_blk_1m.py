a47cbd82bb150ec83404d9a9f7d7f6a96947d92a
Piotr Dziekan <pdziekanp@gmail.com>
2025-09-10 10:50:17 +0200

    blk_1m api test: update warm tests with th_std and const_p

--- tests/python/unit/api_blk_1m.py
+++ tests/python/unit/api_blk_1m.py
@@ -25,7 +25,18 @@ print("melA =", opts.melA)
 print("melB =", opts.melB)
 print("r_c0 =", opts.r_c0)
 print("r_eps =", opts.r_eps)
+print("adj_nwtrph =", opts.adj_nwtrph)
+print("th_dry =", opts.th_dry)
+print("const_p =", opts.const_p)
 
+test_cases = { \
+  "RK4, variable pressure and dry theta" : [0, 1, 0], # adj_nwtrph, th_dry, const_p 
+  "RK4, constant pressure and 'standard' theta" : [0, 0, 1],
+  "Newton-Raphson, variable pressure and dry theta" : [1, 1, 0],
+  "Newton-Raphson, constant pressure and 'standard' theta" : [1, 0, 1],
+}
+
+# test saturation adjustment
 rhod = arr_t([1.  ])
 p    = arr_t([1.e5])
 th   = arr_t([300.])
@@ -35,63 +46,61 @@ rr   = arr_t([0.  ])
 dt   = 1
 dz   = 1
 
-# sat adjustment with variable pressure
-th_old = th.copy()
-rv_old = rv.copy()
-rc_old = rc.copy()
-rr_old = rr.copy()
-blk_1m.adj_cellwise(opts, rhod, th, rv, rc, rr, dt)
-assert th != th_old # some water should have evaporated
-assert rv != rv_old
-assert rc != rc_old
-assert rr == rr_old
+def test_sat_adj(opts, name):
+  print("Testing saturation adjustment with " + name)
+  th_new = th.copy()
+  rv_new = rv.copy()
+  rc_new = rc.copy()
+  rr_new = rr.copy()
+  blk_1m.adj_cellwise(opts, rhod, p, th_new, rv_new, rc_new, rr_new, dt)
+  assert th != th_new # some water should have evaporated
+  assert rv != rv_new
+  assert rc != rc_new
+  assert rr == rr_new
 
-# sat adjustment with constant pressure
-th   = arr_t([300.])
-rv   = arr_t([0.  ])
-rc   = arr_t([0.01])
-rr   = arr_t([0.  ])
-blk_1m.adj_cellwise_constp(opts, rhod, p, th, rv, rc, rr, dt)
-assert th != th_old # some water should have evaporated
-assert rv != rv_old
-assert rc != rc_old
-assert rr == rr_old
+for name, opt in test_cases.items():
+  opts.adj_nwtrph = opt[0]
+  opts.th_dry = opt[1]
+  opts.const_p = opt[2]
+  test_sat_adj(opts, name)
 
-# sat adjustment using Newton-Raphson under constant pressure
-th   = arr_t([300.])
-rv   = arr_t([0.  ])
-rc   = arr_t([0.01])
-blk_1m.adj_cellwise_nwtrph(opts, p, th, rv, rc, dt)
-assert th != th_old # some water should have evaporated
-assert rv != rv_old
-assert rc != rc_old
-assert rr == rr_old
-
-dot_rc = arr_t([0.])
-dot_rr = arr_t([0.])
-blk_1m.rhs_cellwise(opts, dot_rc, dot_rr, rc, rr)
-assert dot_rc != 0 # some water should have coalesced
-assert dot_rr != 0
-
-# forcing using Newton-Raphson saturation adjustment include rain evaporation
-rr   = arr_t([0.01])
+# test RHS cellwise
 dot_th = arr_t([0.])
 dot_rv = arr_t([0.])
 dot_rc = arr_t([0.])
 dot_rr = arr_t([0.])
 
-blk_1m.rhs_cellwise_nwtrph(opts, dot_th, dot_rv, dot_rc, dot_rr, rhod, p, th, rv, rc, rr, dt)
-assert dot_rc != 0 # some water should have coalesced
-assert dot_rr != 0
-assert dot_th != 0 # some rain should have evaporated
-assert dot_rv != 0
+def test_rhs_cell(opts, name):
+  print("Testing RHS cellwise with " + name)
+  dot_th_new = dot_th.copy()
+  dot_rv_new = dot_rv.copy()
+  dot_rc_new = dot_rc.copy()
+  dot_rr_new = dot_rr.copy()
+  if opts.adj_nwtrph:
+    rr = arr_t([0.01])
+    blk_1m.rhs_cellwise_revap(opts, dot_th_new, dot_rv_new, dot_rc_new, dot_rr_new, rhod, p, th, rv, rc, rr, dt)
+    assert dot_th_new != 0 # some rain should have evaporated
+    assert dot_rv_new != 0
+  else:
+    rr = arr_t([0.00])
+    blk_1m.rhs_cellwise(opts, dot_rc_new, dot_rr_new, rc, rr)
+  assert dot_rc_new != 0 # some water should have coalesced
+  assert dot_rr_new != 0
 
+for name, opt in test_cases.items():
+  opts.adj_nwtrph = opt[0]
+  opts.th_dry = opt[1]
+  opts.const_p = opt[2]
+  test_rhs_cell(opts, name)
+
+# test RHS columnwise
 rr   = arr_t([0.])
 dot_rr_old = dot_rr.copy()
 flux = blk_1m.rhs_columnwise(opts, dot_rr, rhod, rr, dz)
 assert flux == 0
 assert dot_rr == dot_rr_old # no rain water -> no precip
 
+#test ice, TODO: separate test?
 th   = arr_t([230.])  #testing ice physics
 ria = arr_t([0.1])
 rib = arr_t([0.1])
